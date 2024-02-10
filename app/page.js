c89a35d698e3ab09c2b5238b29a0e9002f63baa1
jazir788 <jazirsx5@gmail.com>
2024-02-10 12:46:55 +0000

    Added in media queries for navbar

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-
+import Navbar from '@/src/components/Navbar/Navbar';
+import './page.css'
 export default function Home() {
 
   return (
-    <div>
+    <div className="app">
+      <Navbar />
     </div>
   );
 }
