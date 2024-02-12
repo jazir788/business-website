71a1d36936af6d52f501cf2322cf93f3763a82be
jazir788 <jazirsx5@gmail.com>
2024-02-12 21:52:27 +0000

    Added Mobile Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import {BiMenuAltRight} from 'react-icons/bi'
 
 const Navbar = () => {
   return (
     <div className='n-wrapper'>
+
         <div className='container'>
             <div className='n-container'> 
                 <div className='n-logo'>
@@ -22,11 +24,25 @@ const Navbar = () => {
                     </div>
                 </div>
 
-        
             </div>
     
-    </div>    
-      
+        </div>
+    
+        <div className='nm-container'>
+            <span>Test</span>
+
+            <BiMenuAltRight 
+            size={30}/>
+        
+        <div className='nm-menu'>
+
+            <span>What we do</span>
+            <span>How it works</span>                        
+            <span>Who we invest in</span>
+            <span>Testimonials</span>
+            <div className='m-funded-button'>Get Funded</div>
+        </div>
+        </div>
     </div>
   )
 }
