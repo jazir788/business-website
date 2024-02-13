28085f64862aa91708d5db0d714b6dfccc763688
jazir788 <jazirsx5@gmail.com>
2024-02-13 20:44:43 +0000

    Completed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import './Navbar.css'
 import {BiMenuAltRight} from 'react-icons/bi'
+import {RxCross2} from 'react-icons/rx'
 
 const Navbar = () => {
+    const [mobileMenuOpened, setMobileMenuOpened] = useState(false)
   return (
     <div className='n-wrapper'>
 
@@ -29,12 +33,16 @@ const Navbar = () => {
         </div>
     
         <div className='nm-container'>
-            <span>Test</span>
+            <span>Test Mobile</span>
 
-            <BiMenuAltRight 
-            size={30}/>
-        
-        <div className='nm-menu'>
+        {  
+            !mobileMenuOpened ? 
+            <BiMenuAltRight size={30} onClick={()=> setMobileMenuOpened(true)}/>
+            :<RxCross2 size={30} onClick={()=> setMobileMenuOpened(false)} />
+        }
+
+        <div className='nm-menu'
+        style={{transform: mobileMenuOpened && "translateX(0%)" }}>
 
             <span>What we do</span>
             <span>How it works</span>                        
