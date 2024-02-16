62ce32c4667dbacd6cdbb826b00095c3e6fb66ba
jazir788 <jazirsx5@gmail.com>
2024-02-16 20:16:32 +0000

    Created Template Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='h-wrapper'>
+        <div className='container'>
+            <div className='h-container'>
+                Hero Section
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
