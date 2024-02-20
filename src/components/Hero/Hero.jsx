a7979eb9671d05e3ffde839c3ed919b2fa2fea17
jazir788 <jazirsx5@gmail.com>
2024-02-20 16:17:04 +0000

    Created the right side of the hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,25 @@ const Hero = () => {
     <div className='h-wrapper'>
         <div className='container'>
             <div className='h-container'>
-                Hero Section
+            <div className='h-left'>
+
+            </div>
+
+            <div className='h-right'>
+            <div className='h-title'>
+            <span>Redefine How</span>
+            <span>You Grow your</span>
+            <span>Digital Business</span>
+            </div>
+
+            <div className='h-desc'>
+                Revenue based funding and execution support designed for early stage founders
+            </div>
+
+            <span>Email box</span>
+            </div>
+            
+
             </div>
         </div>
     </div>
