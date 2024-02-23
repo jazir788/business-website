ded3e45145b15b38d549c819513151ef400473fa
jazir788 <jazirsx5@gmail.com>
2024-02-23 20:22:36 +0000

    Created left side of hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import './Hero.css'
 import EmailBox from '../EmailBox/EmailBox'
+import { HeroData } from '@/src/utils/data'
 
 const Hero = () => {
   return (
     <div className='h-wrapper'>
         <div className='container'>
             <div className='h-container'>
-            <div className='h-left'>
+              <div className='h-left'>
+
+                <div className='image-row'>
+                {
+                  HeroData.slice(0, 3).map((person, i) => (
+                    <div className='person-pill' key={i}>
+                      <div className='person-pill-bg'>
+                        <img src={person.src} alt={person.src} />
+                      </div>
+                    </div>
+                  ))
+                }
+                </div>
+
+                <div className='image-row'>
+                {
+                  HeroData.slice(3, 6).map((person, i) => (
+                    <div className='person-pill' key={i}>
+                      <div className='person-pill-bg'>
+                        <img src={person.src} alt={person.src} />
+                      </div>
+                    </div>
+                  ))
+                }
+                
+                </div>
 
             </div>
 
