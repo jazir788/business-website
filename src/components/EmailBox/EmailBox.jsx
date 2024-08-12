8abd01192401de013cc07d3ab41be0250b13b800
jazir788 <jazirsx5@gmail.com>
2024-08-12 13:37:29 +0100

    Completed animation on Hero Section

--- src/components/EmailBox/EmailBox.jsx
+++ src/components/EmailBox/EmailBox.jsx
@@ -1,25 +1,61 @@
 import React from 'react'
 import './EmailBox.css'
 import {LuMail} from 'react-icons/lu'
+import { motion } from 'framer-motion'
+import { containerVariants } from '@/src/utils/animations'
 
 const EmailBox = () => {
   return (
-    <div className='emailBox'>
+    <motion.div
+    initial= {{
+      width: ".5rem",
+      borderRadius:"100%"
+    }}
+    whileInView={{
+      width: "70%",
+      borderRadius: "999px",
+      transition:{
+        type: "easeOut",
+        duration: 1
+      }
 
-        <div>
+    }} 
+    
+    className='emailBox'>
+
+        <motion.div
+        variants={containerVariants(0.6)}
+        initial= "offscreen"
+        whileInView={"onscreen"}
+        viewport={{
+          once:true
+        }}>
             <LuMail size={30} color='grey' />
-        </div>
+        </motion.div>
 
-        <input type='email'
+        <motion.input
+        variants={containerVariants(0.7)}
+        initial= "offscreen"
+        whileInView={"onscreen"}
+        viewport={{
+          once:true
+        }} 
+        type='email'
             placeholder='Enter your email' />
 
-        <div className='getFunded'>
+        <motion.div
+        variants={containerVariants(0.9)}
+        initial= "offscreen"
+        whileInView={"onscreen"}
+        viewport={{
+          once:true
+        }} className='getFunded'>
             Get Funded
-        </div>
+        </motion.div>
 
 
       
-    </div>
+    </motion.div>
   )
 }
 
