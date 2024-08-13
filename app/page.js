564d278fc23e980bbea8a238e425f314ffc912e4
jazir788 <jazirsx5@gmail.com>
2024-08-13 08:49:01 +0100

    Added Branding Video

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Navbar from '@/src/components/Navbar/Navbar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
+import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 export default function Home() {
 
   return (
     <div className="app">
       <Navbar />
       <Hero />
+      <BrandingVideo />
     </div>
   );
 }
