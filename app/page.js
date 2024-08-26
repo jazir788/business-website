85d025b472717b2f0e5adc937c87e5559d1ec2d7
jazir788 <jazirsx5@gmail.com>
2024-08-26 22:45:56 +0100

    Created Template OurDiff Page

--- app/page.js
+++ app/page.js
@@ -3,14 +3,16 @@ import './page.css'
 import Hero from '@/src/components/Hero/Hero';
 import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 import WhatWeDo from '@/src/components/WhatWeDo/WhatWeDo';
+import OurDiff from '@/src/components/OurDiff/OurDiff';
 export default function Home() {
 
   return (
     <div className="app">
       <Navbar />
       <Hero />
-      <BrandingVideo />
+      <BrandingVideo />a
       <WhatWeDo />
+      <OurDiff />
     </div>
   );
 }
