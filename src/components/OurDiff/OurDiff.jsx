326128f279e648c429d2c8cc8accb01b3555aa9b
jazir788 <jazirsx5@gmail.com>
2024-08-28 19:07:56 +0100

    Added content to OurDiff Section

--- src/components/OurDiff/OurDiff.jsx
+++ src/components/OurDiff/OurDiff.jsx
@@ -4,6 +4,18 @@ import './OurDiff.css'
 const OurDiff = () => {
   return (
     <div className='od-wrapper'>
+        <div className='container'>
+            <div className='od-container'>
+                <div className='od-head'>
+                    <span className='tag'>Our Difference</span>
+                    <span className='title'>Fair Capitle, hassle free</span>
+                    <span className='text'> Our mission is to level the playing field for early stage growth capital.
+                    <br /> We provide capital that is unbiased, flexible and non dilutive with the execution of support to
+                    accelerate value creation.</span>
+                </div>
+            </div>
+        </div>
+
       
     </div>
   )
