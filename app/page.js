f488a6b016d5f062c6bce8229fbbd9b112a2f724
Jazir Shine <jazirsx5@gmail.com>
2024-10-02 22:06:38 +0100

    Added Who We Invest Section

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ import WhatWeDo from '@/src/components/WhatWeDo/WhatWeDo';
 import OurDiff from '@/src/components/OurDiff/OurDiff';
 import { motion, useAnimation } from 'framer-motion';
 import HowItWorks from '@/src/components/HowItWorks/HowItWorks';
+import WhoWeInvest from '@/src/components/WhoWeInvest/WhoWeInvest';
 export default function Home() {
   const controls = useAnimation()
 
@@ -26,9 +27,22 @@ export default function Home() {
       })}
       viewport={{amount: 0.4}}
       >
-      </motion.div>
       <OurDiff />
+      </motion.div>
       <HowItWorks />
+
+      <motion.div
+      onViewportEnter={()=> controls.start({
+        backgroundColor: "var(--primary-color)",
+      })}
+      onViewportLeave={()=> controls.start({
+        backgroundColor: "white",
+      })}
+      viewport={{amount: 0.4}}
+      >
+      <WhoWeInvest />
+      </motion.div>
+
     </motion.div>
   );
 }
--- src/components/WhoWeInvest/WhoWeInvest.jsx
+++ src/components/WhoWeInvest/WhoWeInvest.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './WhoWeInvest.css'
+import { features, whoWeInvest } from '@/src/utils/data'
 
 const WhoWeInvest = () => {
   return (
-    <div>
+    <div className='wwi-wrapper'>
+        <div className='container'>
+            <div className='wwi-container'>
+            <div className='wwi-left'>
+                <div className='head'>
+                    <span className='tag'>Who we invest in</span>
+                    <span className='title'> Digital Business 
+                    <br /> with early traction </span>
+                </div>
+
+                <div className='wwi-features'>
+                    { 
+                        whoWeInvest.map((feature, i) => (
+                        <div className='wwi-feature' key={i}>
+                            <span className='des'>{feature.title}</span>
+                            <span className='text'>{feature.des}</span>
+                        </div>
+                        ))
+                    }
+                </div>
+            
+            </div>
+
+
+            <div className='wwi-right'>
+                <img src='persons.png' alt='person' />
+            </div>
+
+            </div>
+        </div>
       
     </div>
   )
