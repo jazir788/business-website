e609378fa712597c75e25ffa51f5c95ddaab53be
Jazir Shine <jazirsx5@gmail.com>
2024-10-08 13:30:30 +0100

    Added content to Testimonials Section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './Testimonials.css';
+import SlickSlider from './SlickSlider';
 
 const Testimonials = () => {
   return (
-    <div>
+    <div className='t-wrapper'>
+        <div className='container'>
+            <div className='t-container'>
+                <div className='t-head'>
+                    <span className='tag'>Testimonials</span>
+                    <span className='title'>Access capital that compliments traditional funding options </span>
+                    <span className='des'>What people say about us </span>
+                
+                </div>
+            
+            </div>
+
+            <SlickSlider />
+        
+        </div>
         Testimonials
     </div>
   )
